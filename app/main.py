86219a5c39830ddc200f0d5db0a8ba8f871e51da
tayofish <103644199+tayofish@users.noreply.github.com>
2023-10-03 21:14:26 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-import os, sys
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent_dir)
+import *
 
 from app.baseEncryption import *
 from fastapi import FastAPI, Header, HTTPException, Depends
